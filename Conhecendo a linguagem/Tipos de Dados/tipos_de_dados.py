d293221066e16fcafe154a42e5f3ad3646771aa4
joao candido <joan.jb56@gmail.com>
2024-06-08 20:22:39 -0300

    Types of data

---
+++ Conhecendo a linguagem/Tipos de Dados/tipos_de_dados.py
@@ -0,0 +1,15 @@
+# texto        str
+# numerico     int , float , complex
+# sequencia    list, tuple , range
+# mapa         dict
+# colecao      set, fronzenset
+# booleano     bool
+# binario      bytes, bytearray, memoryview   
+
+print(10 + 10)
+print(5.5)
+print(True)
+print(False)
+print("string")
+
+
