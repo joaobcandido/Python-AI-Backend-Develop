5c026ffac5a8ce09568b0afa43c8d5564f0b2bda
joao candido <joan.jb56@gmail.com>
2024-06-08 21:01:46 -0300

    Interactive mode and local documentation

---
+++ Conhecendo a linguagem/Modo interativo/modo_interativo.py
@@ -0,0 +1,9 @@
+# para entrar no modo interativo digite python no terminal
+# para sair do modo interativo digite exit() no terminal
+
+dir() # retorna a lista de nomes no escopo local
+#['__annotations__', '__builtins__', '__doc__', '__loader__', '__name__', '__package__', '__spec__']
+dir(23) # retorna uma lista de atributos validos para o objeto 23
+# ['__abs__', '__add__', '__and__', '__bool__', '__ceil__', '__class__', '__delattr__', '__dir__', '__divmod__', '__doc__', '__eq__', '__float__', '__floor__', '__floordiv__', '__format__', '__ge__', '__getattribute__', '__getnewargs__', '__getstate__', '__gt__', '__hash__', '__index__', '__init__', '__init_subclass__', '__int__', '__invert__', '__le__', '__lshift__', '__lt__', '__mod__', '__mul__', '__ne__', '__neg__', '__new__', '__or__', '__pos__', '__pow__', '__radd__', '__rand__', '__rdivmod__', '__reduce__', '__reduce_ex__', '__repr__', '__rfloordiv__', '__rlshift__', '__rmod__', '__rmul__', '__ror__', '__round__', '__rpow__', '__rrshift__', '__rshift__', '__rsub__', '__rtruediv__', '__rxor__', '__setattr__', '__sizeof__', '__str__', '__sub__', '__subclasshook__', '__truediv__', '__trunc__', '__xor__', 'as_integer_ratio', 'bit_count', 'bit_length', 'conjugate', 'denominator', 'from_bytes', 'imag', 'is_integer', 'numerator', 'real', 'to_bytes']
+help() # sistema de ajuda integrado para fazer buscas em modo interativo
+# para sair da documentacao digite q
