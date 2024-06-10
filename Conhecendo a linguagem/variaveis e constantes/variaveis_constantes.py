ccdbed92691e32f254967555de5923eef2f84eb0
joao candido <joan.jb56@gmail.com>
2024-06-09 21:07:41 -0300

    variaveis e constantes

---
+++ Conhecendo a linguagem/variaveis e constantes/variaveis_constantes.py
@@ -0,0 +1,11 @@
+# em python nao tem palavra reservada para informar ao interpretador que o 
+#valor e uma constante , entao usamos a convencao de escrever a variavel 
+#toda em letras maiusculas para dizer que e uma constante.
+# exemplo
+DEBUGE = True
+STATE = SP
+# BOAS PRATICAS
+# o padrao de nomes deve ser snake case exemplo: nome_proprio
+# escolher nomes sugestivos
+# nome de constantes todo em  maiusculos
+
