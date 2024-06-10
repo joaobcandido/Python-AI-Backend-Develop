5d8957a856175638d318d3f22efd01d47635ae18
joao candido <joan.jb56@gmail.com>
2024-06-10 13:23:42 -0300

    Type Conversion

---
+++ Conhecendo a linguagem/Conversao de tipos/conversao_de_tipos.py
@@ -0,0 +1,21 @@
+# converter de inteiro para float
+preco = 10
+preco = float(preco)
+print(preco)
+
+# converter float para inteiro
+preco = 5.4
+preco = int(preco)
+print(preco)
+
+# converter inteiro para string
+preco = 12
+print(str(preco))
+
+# concatenando
+idade = 34
+texto = f"a minha idade eh : {idade}"
+print(texto)
+
+# mostrando o tipo da variavel
+print(type(texto))
