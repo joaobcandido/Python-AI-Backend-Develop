cf8887a0134d973af5c4dc8b960ccca7bbba1232
joao candido <joan.jb56@gmail.com>
2024-06-11 12:53:09 -0300

    Code Versioning

---
+++ Versionamento com Git e Github/o que e versionamento de codigo/versionamento_de_codigo.py
@@ -0,0 +1,7 @@
+# o que e versionamento de codigo faz?
+    # registra o histirico de atualizacao de um arquivo
+    # gerencia quem podo modificar o arquivo
+
+# tipos de controle de versao:
+    # VCS Centralizado (CVCS) ex: CVS, Subversion
+    # VCS Distribuido  (DVCS) ex: git, mercurial
