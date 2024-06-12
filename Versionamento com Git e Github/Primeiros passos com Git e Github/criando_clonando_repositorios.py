5875729cf6017dd958499268d29a2a2ebd3aca7e
joao candido <joan.jb56@gmail.com>
2024-06-11 21:20:25 -0300

    criando e clonando

---
+++ Versionamento com Git e Github/Primeiros passos com Git e Github/criando_clonando_repositorios.py
@@ -0,0 +1,5 @@
+# criando repositorio remoto apartir da pasta local
+# usamos o comando : git remoto add origin url
+
+# clonando um repositorio remoto no pc local
+# usamos o comando : git clone url
