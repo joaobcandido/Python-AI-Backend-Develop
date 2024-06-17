ab1daa6451238cb926bbf969b6e160e4655c2077
joao candido <joan.jb56@gmail.com>
2024-06-17 20:47:03 -0300

    comparison operators

---
+++ Tipos de Operadores/operadores_comparacao.py
@@ -0,0 +1,13 @@
+#operadores de comparação são utilizados para comparar dois valores
+saldo = 300
+saque = 200
+# operador de igualdade
+print("saldo eh igual a saque ?",saldo == saque)
+# operador de diferença
+print("saldo eh diferente de saque ?",saldo != saque)
+# operador maior que/ maior ou igual
+print("saldo eh maior de saque ?",saldo > saque)
+print("saldo eh maior ou igual a saque ?",saldo >= saque)
+# operador menor que/ menor ou igual
+print("saldo eh menor de saque ?",saldo < saque)
+print("saldo eh menor ou igual a saque ?",saldo <= saque)
