57b06d998f43b2fe1cee0ffea6d2a8f30f0f2866
joao candido <joan.jb56@gmail.com>
2024-06-17 20:23:10 -0300

    arithmetic operators

---
+++ Tipos de Operadores/operadores_aritimeticos.py
@@ -0,0 +1,22 @@
+# conhecendo os operadores aritimeticos
+# adição
+print("adicao 1 + 1 = ",1 + 1)
+# subtração
+print("subtracao 2 - 1 = ",2 - 1)
+# multiplicação
+print("multiplicacao 2 * 2 = ",2 * 2)
+# divisão
+print("divisao 10 / 3 = ",10 / 3)
+# divisão inteira
+print("divisao inteira 10 // 3 = ",10 // 3)
+# módulo (resto da divisão)
+print("modulo 10 % 3 = ",10 % 3)
+# exponenciação
+print("exponenciacao 2 ** 3 = ",2 ** 3)
+
+###########################
+# precedencia dos operadores
+#1- parêntesis
+#2- expoênte
+#3- multiplicação e divisão (esquerda para direita)
+#4 - soma e subtração (esquerda para direita)
