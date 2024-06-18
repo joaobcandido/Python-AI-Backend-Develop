d0e3979395bdefa09b883b8403ad67491ff8f6d2
joao candido <joan.jb56@gmail.com>
2024-06-18 19:55:09 -0300

    operators with assignment

---
+++ Tipos de Operadores/operadores_atribuicao.py
@@ -0,0 +1,12 @@
+# operadores de atribuição
+saldo = 500
+print("saldo inicial =",saldo)
+# atribuicao com soma
+saldo += 200
+print("saldo + 200 = ",saldo)
+
+# atribuicao com subtracao
+saldo -= 200
+print("saldo - 200 = ",saldo)
+
+# podemos fazer o mesmo usando todos os operadores aritimeticos.
