8d15117304928485f1cc965f648576ead72d659c
joao candido <joan.jb56@gmail.com>
2024-06-20 19:19:08 -0300

    association operators

---
+++ Tipos de Operadores/operadores_associacao.py
@@ -0,0 +1,9 @@
+# Os operadores de associação (in) e (not in) são utilizados para verificar se um
+#  objeto esta presente ou nao em uma sequencia
+
+curso = "Curso de Python"
+frutas = ["laranja", "uva", "limão"] 
+saques = [1500, 100]
+
+print("Python" in curso)
+print("python"  not in curso)
