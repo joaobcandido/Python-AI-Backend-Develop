7bc50a21d6dbee7a7626ab54f9fa85f9488dd9e7
joao candido <joan.jb56@gmail.com>
2024-06-20 20:31:48 -0300

    block identification

---
+++ Estrutura Condicionais e de Repeticao/identacao_blocos.py
@@ -0,0 +1,8 @@
+# identar código é uma forma de manter o código fonte mais legivel
+# metodo sacar
+def sacar(valor):
+    saldo = 500
+    if saldo >= valor:
+        print("valor sacado!!")
+
+sacar(499)
