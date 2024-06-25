22dcf92b2bdc65f8d9b40005731856c34d394eca
joao candido <joan.jb56@gmail.com>
2024-06-25 19:03:34 -0300

    string class methods

---
+++ Manipulacao de strings/metodos_classe_string.py
@@ -0,0 +1,18 @@
+curso = "pyThOn"
+# metodo upper deixa todos os caracteres maiusculos
+print(curso.upper())
+# metodo lower deixa todos os caracteres minusculos
+print(curso.lower())
+# metodo title deixa o primeiro caractere maiusculos e o resto minusculo
+print(curso.title())
+# metodo strip retira os espacos em branco do inicio e do fim da cadeia de caracteres
+print(curso.strip())
+# metodo lstrip remove os espacos em branco do comeco da string
+print(curso.lstrip())
+# metodo rstrip remove os espacos em branco do final da string
+print(curso.rstrip())
+# metodo center para centralizar 
+print(curso.center(10," "))
+# metodo join adiciona um caractere interavel
+print("-".join(curso))
+
