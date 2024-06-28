4a61b2d810ab2695bb59db82c8e94186c631f82a
joao candido <joan.jb56@gmail.com>
2024-06-27 21:05:49 -0300

    lists

---
+++ Estrutura de dados/Trabalhando com listas/declarando_listas.py
@@ -0,0 +1,20 @@
+frutas = ["laranja", "maca", "uva"]
+print(frutas)
+
+frutas2 = []
+print(frutas)
+
+letras = list("python")
+print(letras)
+
+numeros = list(range(10))
+print(numeros)
+# a lista pode conter varios objetos diferentes como string , inteiros ,booleano e float
+carro = ["Ferrari", "F8", 4200000, 2020, 2900, "São Paulo", True]
+print(carro)
+
+# assesando os valores pelo indice positivo que comeca em (0)
+print(frutas[0]) # laranja
+
+# assesando os valores da lista pelo indice negativo que comeca do -1
+print(frutas[-3]) # laranja
