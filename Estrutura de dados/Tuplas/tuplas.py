919542ebd1f0403e515e4f272b6b36d0b8041861
joao candido <joan.jb56@gmail.com>
2024-07-01 19:28:22 -0300

    tuple

---
+++ Estrutura de dados/Tuplas/tuplas.py
@@ -0,0 +1,16 @@
+# a diverenca entre tupla e lista eh que a tupla nao pode ser modificada e eh declarada dentro de parenteses
+frutas = (
+    "laranja",
+    "pera",
+    "uva",
+)
+print(frutas)
+
+letras = tuple("python")
+print(letras)
+
+numeros = tuple([1, 2, 3, 4])
+print(numeros)
+
+pais = ("Brasil",)
+print(pais)
