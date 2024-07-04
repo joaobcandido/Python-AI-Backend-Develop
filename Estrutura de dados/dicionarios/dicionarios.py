c04a53861d912b06ef55da83e6e86a1ec15657f6
joao candido <joan.jb56@gmail.com>
2024-07-03 21:24:28 -0300

    methods used with dictionary

---
+++ Estrutura de dados/dicionarios/dicionarios.py
@@ -0,0 +1,171 @@
+print("""
+dicionarios sao delimitados por {}, e contem uma lista de
+pares chave:valor separados por virgulas
+    """)
+
+pessoa = {"nome": "Guilherme", "idade": 28}
+print(pessoa)
+
+pessoa = dict(nome="Guilherme", idade=28)
+print(pessoa)
+
+pessoa["telefone"] = "3333-1234"  # {"nome": "Guilherme", "idade": 28, "telefone": "3333-1234"}
+print(pessoa)
+
+#acessando dados do dicionario
+dados = {"nome": "Guilherme", "idade": 28, "telefone": "3333-1234"}
+# para acessa um valor do dicionario precisamos passa a chave correspondente
+print(dados["nome"])  # "Guilherme"
+print(dados["idade"])  # 28
+print(dados["telefone"])  # "3333-1234"
+
+dados["nome"] = "Maria"
+dados["idade"] = 18
+dados["telefone"] = "9988-1781"
+
+print(dados)  # {"nome": "Maria", "idade": 18, "telefone": "9988-1781"}
+
+# dicionarios aninhados
+contatos = {
+    "guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"},
+    "giovanna@gmail.com": {"nome": "Giovanna", "telefone": "3443-2121"},
+    "chappie@gmail.com": {"nome": "Chappie", "telefone": "3344-9871"},
+    "melaine@gmail.com": {"nome": "Melaine", "telefone": "3333-7766"},
+}
+
+telefone = contatos["giovanna@gmail.com"]["telefone"]  # "3443-2121"
+print(telefone)
+
+for chaves in contatos:
+    print(chaves,contatos[chaves])
+
+print("=" * 100)    
+
+for chave, valor in contatos.items():
+    print(chave, valor)
+
+print("=" * 100) 
+# metodo clear para limpar o dicionario
+contatos.clear()
+print(contatos)  # {}
+
+print("=" * 100) 
+# metodo copy para copiar um dicionario
+contatos = {"guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"}}
+
+copia = contatos.copy()
+copia["guilherme@gmail.com"] = {"nome": "Gui"}
+
+print(contatos["guilherme@gmail.com"])  # {"nome": "Guilherme", "telefone": "3333-2221"}
+
+print(copia["guilherme@gmail.com"])  # {"nome": "Gui"}
+
+# utilizamos fromkeys para adicionar chaves e tambem um valor padrao para esse chaves em um  dicionario existente ou nao
+resultado = dict.fromkeys(["nome", "telefone"])  # {"nome": None, "telefone": None}
+print(resultado)
+
+resultado = dict.fromkeys(["nome", "telefone"], "vazio")  # {"nome": "vazio", "telefone": "vazio"}
+print(resultado)
+
+# utilizamos o metodo get para buscar um valor dentro do dicionario ,mais se ele nao encontrar a chave
+# ele retorna (None) 
+# temos um funcionalidade dentro do get que podemos passar um valor default como seguindo argumento
+# entao se ele nao encontra a chave ele devolve um dicionario vazio 
+# e com isso nao deixamos que ocorra um KeyError
+contatos = {"guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"}}
+
+# contatos["chave"]  # KeyError
+
+resultado = contatos.get("chave")  # None
+print(resultado)
+
+resultado = contatos.get("chave", {})  # {}
+print(resultado)
+
+resultado = contatos.get(
+    "guilherme@gmail.com", {}
+)  # {"guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"}
+print(resultado)
+
+# utilizamos o item para retornar uma lista de tuplas
+contatos = {"guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"}}
+
+resultado = contatos.items()  # dict_items([('guilherme@gmail.com', {'nome': 'Guilherme', 'telefone': '3333-2221'})])
+print(resultado)
+
+# utilizamos Keys para saber quais sao as chaves presente no dicionario
+contatos = {"guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"}}
+
+resultado = contatos.keys()  # dict_keys(['guilherme@gmail.com'])
+print(resultado)
+
+print("#" *100)
+# utilizamos o pop para remover uma chave de um dicionario
+contatos = {"guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"}}
+
+resultado = contatos.pop("guilherme@gmail.com")  # {'nome': 'Guilherme', 'telefone': '3333-2221'}
+print(resultado)
+# podemos passar um segundo argumento para o metodo pop ,como isso devolve um dicionario vazio
+# em vez de um KeyError: 'guilherme@gmail.com'
+resultado = contatos.pop("guilherme@gmail.com",{})  # {}
+print(resultado)
+
+# podemos utilizar o popitem para remover  as chaves na sequencia
+contatos = {"guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"}}
+
+resultado = contatos.popitem()  # ('guilherme@gmail.com', {'nome': 'Guilherme', 'telefone': '3333-2221'})
+print(resultado)
+
+# contatos.popitem()  # KeyError
+print("#" *100)
+# podemos utilizar o setdefault para adicionar um chave e um valor se eles nao existirem no dicionario
+contato = {"nome": "Guilherme", "telefone": "3333-2221"}
+
+contato.setdefault("nome", "Giovanna")  # "Guilherme"
+print(contato)  # {'nome': 'Guilherme', 'telefone': '3333-2221'}
+
+contato.setdefault("idade", 28)  # 28
+print(contato)  # {'nome': 'Guilherme', 'telefone': '3333-2221', 'idade': 28}
+
+# podemos utilizar o update para atualizar o dicionario
+contatos = {"guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"}}
+
+contatos.update({"guilherme@gmail.com": {"nome": "Gui"}})
+print(contatos)  # {'guilherme@gmail.com': {'nome': 'Gui'}}
+
+contatos.update({"giovanna@gmail.com": {"nome": "Giovanna", "telefone": "3322-8181"}})
+# {'guilherme@gmail.com': {'nome': 'Gui'}, 'giovanna@gmail.com': {'nome': 'Giovanna', 'telefone': '3322-8181'}}
+print(contatos)
+
+# podemos utilizar o values para retornar so os valores contidos no dicionario
+contatos = {
+    "guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"},
+    "giovanna@gmail.com": {"nome": "Giovanna", "telefone": "3443-2121"},
+    "chappie@gmail.com": {"nome": "Chappie", "telefone": "3344-9871"},
+    "melaine@gmail.com": {"nome": "Melaine", "telefone": "3333-7766"},
+}
+
+resultado = (
+    contatos.values()
+)  # dict_values([{'nome': 'Guilherme', 'telefone': '3333-2221'}, {'nome': 'Giovanna', 'telefone': '3443-2121'}, {'nome': 'Chappie', 'telefone': '3344-9871'}, {'nome': 'Melaine', 'telefone': '3333-7766'}])  # noqa
+print(resultado)
+
+# utilizamos a palavra reservada in para sabermos se alguma chave esta contida no dicionario
+resultado = "guilherme@gmail.com" in contatos  # True
+print(resultado)
+
+resultado = "megui@gmail.com" in contatos  # False
+print(resultado)
+
+resultado = "idade" in contatos["guilherme@gmail.com"]  # False
+print(resultado)
+
+resultado = "telefone" in contatos["giovanna@gmail.com"]  # True
+print(resultado)
+
+# utilizamos del para deletar um dicionario ou alguma chave
+del contatos["guilherme@gmail.com"]["telefone"]
+del contatos["chappie@gmail.com"]
+
+# {'guilherme@gmail.com': {'nome': 'Guilherme'}, 'giovanna@gmail.com': {'nome': 'Giovanna', 'telefone': '3443-2121'}, 'melaine@gmail.com': {'nome': 'Melaine', 'telefone': '3333-7766'}}  # noqa
+print(contatos)
